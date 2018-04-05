cccf4d4435152a511c1f19100baa8a5211fec4cb
zliu63 <zliu63@illinois.edu>
2018-04-05 01:21:09 -0500

    ok

--- k_means.py
+++ k_means.py
@@ -12,8 +12,18 @@ Your code should return a 2d array containing
 the centers.
 
 '''
-# Import the dataset
-
+#import dataset
+myMap = {"Iris-setosa":0,"Iris-versicolor":1,"Iris-virginica":2}
+filename = "/Users/ziyangliu/Documents/CS446/KMeansClustering/data/iris.data"
+lines = [line.rstrip('\n').split(",") for line in open(filename,"r")] 
+lines.pop(0)
+data = np.array(lines)
+X = data[:,0:4].astype(np.float)
+Y = data[:,4]
+N = Y.shape[0]
+for i in range(N):
+    Y[i] = myMap[Y[i]]
+Y = Y.astype(np.float)
 # Make 3  clusters
 k = 3
 # Initial Centroids
@@ -22,9 +32,34 @@ C = np.array(C)
 print("Initial Centers")
 print(C)
 
+def choose(x,C_curr):
+    dist = np.square(C_curr-x)
+    dist = np.sum(dist,axis = 1)
+    return np.argmin(dist)
+
 def k_means(C):
-    # Write your code here!
-    return C_final
+    '''
+    Repeat until convergence:
+        2.1 (Recenter.) Set μj := mean(Cj) for j ∈ (1,...,k).
+        2.2 (Reassign). Update Cj := {xi : μ(xi) = μj} for j ∈ (1,...,k)
+        (break ties arbitrarily).
+    '''
+    pred = np.zeros(N)
+    K = C.shape[0]
+    D = C.shape[1]
+    C_next = np.zeros((K,D))
+    C_curr = C
+    for i in range(N):
+        pred[i] = choose(X[i],C_curr)
+    for k in range(K):
+        C_next[k] = np.mean(X[pred==k],axis = 0)
+    while not np.array_equal(C_next,C_curr):
+        C_curr = deepcopy(C_next)
+        for i in range(N):
+            pred[i] = choose(X[i],C_curr)
+        for k in range(K):
+            C_next[k] = np.mean(X[pred==k],axis = 0)            
+    return C_next
 
 
 
